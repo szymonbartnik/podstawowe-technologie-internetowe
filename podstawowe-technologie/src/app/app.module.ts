12452edfb14d66e0a104e56fbfbc9d82f70aa22c
sbartnik <sbartnik@valuelogic.one>
2020-11-05 17:46:41 +0100

    generated students component

--- podstawowe-technologie/src/app/app.module.ts
+++ podstawowe-technologie/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentsComponent } from './students/students.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentsComponent
   ],
   imports: [
     BrowserModule,
